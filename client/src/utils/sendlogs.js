cbe5f903608a411dcdbc167e1900c6029d6a1833
Raja Kedia <raja.kedia@FC-Raja.Kedia>
2023-05-02 13:38:09 +0530

    fix: added cors

--- client/src/utils/sendlogs.js
+++ client/src/utils/sendlogs.js
@@ -2,7 +2,7 @@ import Axios from "axios";
 
 const isClientSide = typeof window !== "undefined";
 
-const URL = "https://3f86-110-173-180-18.ngrok.io/api";
+const URL = "https://4f40-110-173-180-18.ngrok.io/api";
 
 export function sendLogs(log) {
   // Keeping comments to help debug
@@ -13,7 +13,13 @@ export function sendLogs(log) {
       {
         headers: {
           "content-type": "application/json",
+          mode: "cors",
+          'sec-fetch-mode': 'cors',
+          accept: '*/*'
+        
         },
+        mode: "no-cors",
+        
       }
     ).catch((err) => {
       console.log("Err: ", err, log);
